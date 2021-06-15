2b74aa0f2ada58184db2397b91c66096c325dca6
furkanaktepe01 <7.furkanaktepe@gmail.com>
2021-06-15 13:35:30 +0300

    Level.gameIntro() has modified.

--- HiddenWordGame/Level.java
+++ HiddenWordGame/Level.java
@@ -177,13 +177,13 @@ public class Level {
                 "To start the game each of two players should type their names and budgets. Game consists of 3 rounds.\n" +
                 "In the first two rounds one player will type a word and other will try to find it, respectively.\n" +
                 "In the final round both players will try to find a hidden word which is selected by the Casino.\n\n" +
-                "The length of the hidden words and players' choice on the size of the table specifies the hardship of the level. The harder the level, the greater the profit.\n" +
+                "The length of the hidden words and players' choice on the size of the table specifies the hardship of the level. The harder the level, the greater the profit or loss.\n" +
                 "Also, the number of guessing and tip chances depends on the hardship of the level. To avoid inferring the length of the hidden word,\n" +
                 "Players will not know how many chances there are, but will be notified in the last 3 chances.\n" +
-                "Also, extra guessing chances gets converted to extra profit depending on the hardship of the level.\n" +
+                "Also, extra guessing chances gets converted to extra profit depending on the hardship of the level as well as the amount of the bets.\n" +
                 "In case a player make a guess other than the hidden word, if the other player accepts the typed word valid, the player will gain from that word too.\n\n" +
-                "In first two rounds depending on the level, certain amount of the bets made in the beginning of each round will be taken by the winner in the end of the round.\n" +
-                "In the final round the winner takes the bet of the opponent, if both player lose, they both loses their bet.\n"
+                "In the first two rounds, depending on the level, certain amount of the bet of the opponent will be taken by the winner in the end of the round.\n" +
+                "In the final round the winner takes the whole bet of the opponent, if both player lose, they both loses their whole bet.\n"
 
         );
     }
